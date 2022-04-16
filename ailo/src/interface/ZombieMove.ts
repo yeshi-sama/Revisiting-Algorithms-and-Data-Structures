5ccae35cb77fe0c3fa05bf2e1b8bb095d49711f1
yeshi-sama <davidl@b-launcher.com>
2022-04-16 14:56:05 +1000

    removed ailo zombie world

--- ailo/src/interface/ZombieMove.ts
+++
@@ -1,6 +0,0 @@
-import Zombie from "../models/Zombie";
-
-export type ZombieMoveType = {
-    zombie: Zombie,
-    gridSize: number
-} 
--- ailo/src/ui/console/LogDisplay.ts
+++
@@ -1,7 +0,0 @@
-export default (text: string, divide?:boolean) => {
-    if(divide) {
-        console.log("=========================================================");
-        console.log("=========================================================");
-    }
-    console.log(`~~~~> ${text}`)
-}
